01c4bb57d164f81e8b7e1ffbbe4cb616eb4db450
jackpreble <60149852+jackpreble@users.noreply.github.com>
2020-04-08 22:57:26 -0400

    4/8/20 10:57p

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import numpy as np
 
 from drawMap import *
 
+# Numpy tutorial (took stuff from it): https://likegeeks.com/numpy-array-tutorial/#Add-array-element
+
 # using file: "Colorado_480x480.dat"
 
 # Global Variables
@@ -16,6 +18,7 @@ startz = 0  # height of start point
 endz = 0  # height of end point
 startList = []  # values of heights on the far right side of the map
 endList = [] # values of heights on the far left side of the map
+carryOn = True
 
 # Quick Variables
 startx = 480
@@ -24,52 +27,80 @@ endx = 0
 #edit
 
 # Working list of all values (organized into lists by x value)
-xyz = np.loadtxt('Colorado_480x480.dat') # https://www.kaggle.com/questions-and-answers/27699
+data = np.loadtxt('Colorado_480x480.dat') # https://www.kaggle.com/questions-and-answers/27699
 
-
+grid = np.array([])
 
 # fits in somewhere: https://docs.scipy.org/doc/numpy/user/quickstart.html
 
 def average():
     global avg
 
-    avg = np.average(xyz)
+    avg = np.average(data)
 
 
 def findStart():
     global avg, startList, startz
 
+    y = 0
+
     average()
 
-    for i in xyz:
+    for i in data:
         startList.append(int(i[-1]))
 
+
     startz = startList[min(range(len(startList)), key=lambda i: abs(startList[i] - avg))] # https://www.geeksforgeeks.org/python-find-closest-number-to-k-in-given-list/
+
 
 def findEnd():
     global avg, endList, endz
 
     average()
 
-    for i in xyz:
+    for i in data:
         endList.append(int(i[1]))
 
     endz = endList[min(range(len(endList)), key=lambda i: abs(endList[i] - startz))] # https://www.geeksforgeeks.org/python-find-closest-number-to-k-in-given-list/
 
 
+def buildGrid():
+    global grid, data
+
+    add = []
+
+    c = 0
+
+    for i in data[c]:
+            add.append([e])
+
+    for e in add:
+        for i in data:
+            add.append(np.where(data == i))
+
+    print(add)
+
 def main():
-    global gray
+    global carryOn
 
-    findStart()
+    '''findStart()
     findEnd()
-    #draw_map(gray,px=1)
 
-    print(avg)
+    while carryOn:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # https://stackoverflow.com/questions/26822175/pygame-if-event-type-pygame-keydown-typeerror-int-object-is-not-callable/26822211
+                carryOn = False'''
+
+    print(data)
+
+    return(buildGrid())
+
+    '''print(avg)
     print(startList)
     print(endList)
     print(startz)
-    print(endz)
-
+    print(endz)'''
 
 if __name__ == "__main__":
     main()
+
